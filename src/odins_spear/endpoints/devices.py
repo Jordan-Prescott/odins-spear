c5b5db1cb27103566fa1113723b5236ae391d1f7
Billy <null>
2025-03-25 12:58:22 +0000

    stuff

--- src/odins_spear/endpoints/devices.py
+++ src/odins_spear/endpoints/devices.py
@@ -1,4 +1,5 @@
 from .base_endpoint import BaseEndpoint
+from ..utils.formatters import format_filter_value
 
 
 class Devices(BaseEndpoint):
@@ -6,340 +7,118 @@ class Devices(BaseEndpoint):
         super().__init__(*args, **kwargs)
 
     # GET
+    def get_group_devices(
+        self,
+        service_provider_id: str,
+        group_id: str,
+        filter: str = None,
+        filter_type: str = None,
+        filter_value: str = None,
+        limit: int = None,
+    ):
+        """
+        
+        """
 
-    # POST
-    def post_group_device(
+        endpoint: str = "/groups/devices?"
+
+        params: dict = {
+            "serviceProviderId": service_provider_id,
+            "groupId": group_id
+        }
+
+        if filter:
+            params[filter] = format_filter_value(filter_type, filter_value)
+        if limit:
+            params["limit"] = limit
+
+        return self._requester.get(endpoint, params=params)
+    
+    def get_group_device(
         self,
         service_provider_id: str,
         group_id: str,
         device_name: str,
-        device_type: str,
-        payload: dict = {},
+        filter: str = None,
+        filter_type: str = None,
+        filter_value: str = None,
+        limit: int = None,
     ):
-        """Adds a new device to a group.
-
-        Args:
-            service_provider_id (str): Service provider ID where the device should be built.
-            group_id (str): Group ID where the device should be built
-            device_name (str): Name of the new device
-            device_type (str): Type of device.
-            payload (dict, optional): Device configuration data.
-
-        Returns:
-            Dict: Returns the device profile.
         """
-
-        endpoint = "/groups/devices"
-
-        payload["serviceProviderId"] = service_provider_id
-        payload["groupId"] = group_id
-        payload["deviceName"] = device_name
-        payload["deviceType"] = device_type
-
-        return self._requester.post(endpoint, data=payload)
-
-    # PUT
-    def put_group_devices(
-        self, service_provider_id: str, group_id: str, device_name: str, updates: dict
-    ):
-        """Update a single device in a group.
-
-        Args:
-            service_provider_id (str): Service Provider or Enterprise ID where Group is located.
-            group_id (str): Group ID where target device is located.
-            device_name (str): Device name of the target device.
-            updates (dict): Updates to apply to the target device.
-
-        Returns:
-            Dict: Python dictionary of the new state after updates have been applied.
+        
         """
+        
+        endpoint: str = "/groups/devices?"
 
-        endpoint = "/groups/devices"
-
-        updates["serviceProviderId"] = service_provider_id
-        updates["groupId"] = group_id
-        updates["deviceName"] = device_name
-
-        return self._requester.put(endpoint, data=updates)
-
-    def put_service_provider_device(
-        self, service_provider_id: str, device_name: str, updates: dict
-    ):
-        """Update a single device in a Service Provider or Enterprise.
-
-        Args:
-            service_provider_id (str): Service Provider or Enterprise ID where device is located.
-            device_name (str): Device name of the target device.
-            updates (dict): Updates to apply to the target device.
-
-        Returns:
-            Dict: Python dictionary of the new state after updates have been applied.
-        """
-
-        endpoint = "/service-providers/devices"
-
-        updates["serviceProviderId"] = service_provider_id
-        updates["deviceName"] = device_name
-
-        return self._requester.put(endpoint, data=updates)
-
-    def put_system_devices(self, device_name: str, updates: dict):
-        """Update a single device in the Broadworks system.
-
-        Args:
-            device_name (str): Device name of the target device.
-            updates (dict): Updates to apply to the target device.
-
-        Returns:
-            Dict: Python dictionary of the new state after updates have been applied.
-        """
-
-        endpoint = "/service-providers/devices"
-
-        updates["deviceName"] = device_name
-
-        return self._requester.put(endpoint, data=updates)
-
-    def put_system_device_file(self, device_name: str, updates: dict):
-        """Update a config file for a single device at the system level.
-
-        Args:
-            device_name (str): Device name of the target device.
-            updates (dict): Updates to apply to the target device.
-
-        Returns:
-            None: This method does not return any specific value.
-        """
-
-        endpoint = "/system/devices/files"
-
-        updates["deviceName"] = device_name
-
-        return self._requester.put(endpoint, data=updates)
-
-    def put_service_provider_device_file(self, device_name: str, updates: dict):
-        """Update a config file for a single device at Service Provider or
-        Enterprise level.
-
-        Args:
-            device_name (str): Device name of the target device.
-            updates (dict): Updates to apply to the target device.
-
-        Returns:
-            None: This method does not return any specific value.
-        """
-
-        endpoint = "/service_provider/devices/files"
-
-        updates["deviceName"] = device_name
-
-        return self._requester.put(endpoint, data=updates)
-
-    def put_group_device_file(self, device_name: str, updates: dict):
-        """Update a config file for a single device at Group level.
-
-        Args:
-            device_name (str): Device name of the target device.
-            updates (dict): Updates to apply to the target device.
-
-        Returns:
-            None: This method does not return any specific value.
-        """
-
-        endpoint = "/groups/devices/files"
-
-        updates["deviceName"] = device_name
-
-        return self._requester.put(endpoint, data=updates)
-
-    def put_group_device_tags_profile(
-        self, service_provider_id: str, group_id: str, device_name: str, tags: list
-    ):
-        """Update tags assigned to single device at group level.
-
-        Args:
-            service_provider_id (str): Service Provider or Enterprise ID where Group is located.
-            group_id (str): Group ID where target device is located.
-            device_name (str): Device name of the target device.
-            tags (dict): List of dictionaries tag name and value entries.
-
-        Returns:
-            Dict: Python dictionary of the new state after updates have been applied.
-        """
-
-        endpoint = "/groups/devices/profile"
-
-        data = {
+        params: dict = {
             "serviceProviderId": service_provider_id,
             "groupId": group_id,
-            "deviceName": device_name,
-            "tags": [{"elements": tags}],
+            "deviceName": device_name
         }
 
-        return self._requester.put(endpoint, data=data)
+        if filter:
+            params[filter] = format_filter_value(filter_type, filter_value)
+        if limit:
+            params["limit"] = limit
 
-    def put_group_device_tag(
+        return self._requester.get(endpoint, params=params)
+    
+    def get_group_device_available_ports(
         self,
         service_provider_id: str,
         group_id: str,
         device_name: str,
-        tag_name: str,
-        tag_value: str,
+        device_level: str = "Group"
     ):
-        """Update a single tag assigned to a device at group level.
-
-        Args:
-            service_provider_id (str): Service Provider or Enterprise ID where Group is located.
-            group_id (str): Group ID where target device is located.
-            device_name (str): Device name of the target device.
-            tag_name (str): Name of the tag to add or update.
-            tag_value (str): Value of tag to add or update.
-
-        Returns:
-            Dict: Python dictionary of the new state after updates have been applied.
+        """
         """
 
-        endpoint = "/groups/devices/tags"
+        endpoint: str = "/groups/devices/available-ports?"
 
-        data = {
-            "tagName": f"%{tag_name}%",
-            "tagValue": tag_value,
+        params: dict = {
             "serviceProviderId": service_provider_id,
             "groupId": group_id,
             "deviceName": device_name,
+            "deviceLevel": device_level
         }
 
-        return self._requester.put(endpoint, data=data)
-
-    def put_service_provider_device_tag(
-        self, service_provider_id: str, device_name: str, tag_name: str, tag_value: str
+        return self._requester.get(endpoint, params=params)
+    
+    def get_group_devices_available(
+        self,
+        service_provider_id: str,
+        group_id: str
     ):
-        """Update a single tag assigned to a device at the Service Provider or Enterprise level.
-
-        Args:
-            service_provider_id (str): Service Provider or Enterprise ID where Group is located.
-            device_name (str): Device name of the target device.
-            tag_name (str): Name of the tag to add or update.
-            tag_value (str): Value of tag to add or update.
-
-        Returns:
-            Dict: Python dictionary of the new state after updates have been applied.
+        """
         """
 
-        endpoint = "/service-providers/devices/tags"
+        endpoint: str = "/groups/devices?"
 
-        data = {
-            "tagName": f"%{tag_name}%",
-            "tagValue": tag_value,
+        params: dict = {
             "serviceProviderId": service_provider_id,
-            "deviceName": device_name,
+            "groupId": group_id,
+            "q": "available"
         }
 
-        return self._requester.put(endpoint, data=data)
-
-    def put_system_device_tag(self, device_name: str, tag_name: str, tag_value: str):
-        """Update a single tag assigned to a device at the System level.
-
-        Args:
-            device_name (str): Device name of the target device.
-            tag_name (str): Name of the tag to add or update.
-            tag_value (str): Value of tag to add or update.
-
-        Returns:
-            Dict: Python dictionary of the new state after updates have been applied.
-        """
-
-        endpoint = "/system/devices/tags"
-
-        data = {
-            "tagName": f"%{tag_name}%",
-            "tagValue": tag_value,
-            "deviceName": device_name,
-        }
-
-        return self._requester.put(endpoint, data=data)
-
-    def put_group_device_type_file(
-        self, service_provider_id: str, group_id: str, device_type: str, updates: dict
-    ):
-        """Set config file for all devices of a specific type at the group level.
-
-        Args:
-            service_provider_id (str): Service Provider or Enterprise ID where Group is located.
-            group_id (str): Group ID where target device is located.
-            device_type (str): The device type you would like to apply the changes to.
-            updates (dict): Cupdates (dict): Updates to apply to the target device.
-
-        Returns:
-            None: This method does not return any specific value.
-        """
-
-        endpoint = "/groups/device-types/files"
-
-        updates["serviceProviderId"] = service_provider_id
-        updates["groupId"] = group_id
-        updates["deviceType"] = device_type
-
-        return self._requester.put(endpoint, data=updates)
-
-    def put_group_device_type_tag(
+        return self._requester.get(endpoint, params=params)
+    
+    def get_group_device_users(
         self,
         service_provider_id: str,
         group_id: str,
-        device_type: str,
-        tag_name: str,
-        tag_value: str,
+        device_name: str
     ):
-        """Update tags applied to device types at the Group level.
-
-        Args:
-            service_provider_id (str): Service Provider or Enterprise ID where Group is located.
-            group_id (str): Group ID where target device is located.
-            device_type (str): The target device type to apply the updates.
-            tag_name (str): Name of the tag to add or update.
-            tag_value (str): Value of tag to add or update.
-
-        Returns:
-            Dict: Python dictionary of the new state after updates have been applied.
+        """
         """
 
-        endpoint = "/groups/system/device-types/tags"
+        endpoint: str = "/groups/devices/users?"
 
-        data = {
-            "tagName": tag_name,
-            "tagValue": tag_value,
+        params: dict = {
             "serviceProviderId": service_provider_id,
             "groupId": group_id,
-            "deviceType": device_type,
+            "deviceName": device_name
         }
 
-        return self._requester.put(endpoint, data=data)
-
-    def put_service_provider_device_type_tag(
-        self, service_provider_id: str, device_type: str, tag_name: str, tag_value: str
-    ):
-        """Update tags applied to device types at the Service Provider or Enterprise level.
-
-        Args:
-            service_provider_id (str): Service Provider or Enterprise ID where Group is located.
-            device_type (str): The target device type to apply the updates.
-            tag_name (str): Name of the tag to add or update.
-            tag_value (str): Value of tag to add or update.
-
-        Returns:
-            Dict: Python dictionary of the new state after updates have been applied.
-        """
-
-        endpoint = "/service-providers/device-types/tags"
-
-        data = {
-            "tagName": tag_name,
-            "tagValue": tag_value,
-            "serviceProviderId": service_provider_id,
-            "deviceType": device_type,
-        }
-
-        return self._requester.put(endpoint, data=data)
+        return self._requester.get(endpoint, params=params)
 
 
-# DELETE
