86746c1865d0408ff7d72d8b0486b68dad7cd3df
Jordan-Prescott <jprescott23@gmail.com>
2025-06-04 11:56:41 +0100

    GET done

--- src/odins_spear/endpoints/call_pickup.py
+++ src/odins_spear/endpoints/call_pickup.py
@@ -7,6 +7,50 @@ class CallPickup(BaseEndpoint):
 
     # GET
 
+    def get_call_pickup_groups(self, service_provider_id: str, group_id: str):
+        """Retrieves Pickup Group information for the specified group.
+
+        Args:
+            service_provider_id (str): Target Service Provider ID
+            group_id (str): The Target Group ID the user is apart of.
+
+        Returns:
+            Dict: Specified users pickup group, and the users within that group.
+        """
+
+        endpoint = "/groups/call-pickup/groups"
+
+        params = {
+            "serviceProviderId": service_provider_id,
+            "groupId": group_id,
+        }
+
+        return self._requester.get(endpoint, params=params)
+
+    def get_call_pickup_group(
+        self, service_provider_id: str, group_id: str, pickup_group_name: str
+    ):
+        """Retrieves Pickup Group information for the specified group.
+
+        Args:
+            service_provider_id (str): Target Service Provider ID
+            group_id (str): The Target Group ID the user is apart of.
+            pickup_group_name (str): The Target Pickup Group Name
+
+        Returns:
+            Dict: Specified users pickup group, and the users within that group.
+        """
+
+        endpoint = "/groups/call-pickup/groups"
+
+        params = {
+            "serviceProviderId": service_provider_id,
+            "groupId": group_id,
+            "name": pickup_group_name,
+        }
+
+        return self._requester.get(endpoint, params=params)
+
     def get_call_pickup_group_user(
         self, service_provider_id: str, group_id: str, user_id: str
     ):
@@ -31,8 +75,33 @@ class CallPickup(BaseEndpoint):
 
         return self._requester.get(endpoint, params=params)
 
+    def get_call_pickup_available_users(self, service_provider_id: str, group_id: str):
+        """Retrieves available users to assign to a pick up group for the specified group.
 
-# POST
+        Args:
+            service_provider_id (str): Target Service Provider ID
+            group_id (str): The Target Group ID the user is apart of.
+
+        Returns:
+            Dict: Available users to assign to a pick up group for the specified group.
+        """
+
+        endpoint = "groups/call-pickup/users"
+
+        params = {"serviceProviderId": service_provider_id, "groupId": group_id}
+
+        return self._requester.get(endpoint, params=params)
+
+    # POST
+
+    def post_call_pickup_group(
+        self,
+    ):
+        """"""
+        endpoint = "/groups/call-pickup/groups"
+
+        params = {""}
+
 
 # PUT
 
